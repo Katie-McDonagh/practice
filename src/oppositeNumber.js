78c8165f3a1db633638c24d4efb246ac78174142
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-01-17 14:37:51 +0000

    notes

--- src/oppositeNumber.js
+++ src/oppositeNumber.js
@@ -5,7 +5,13 @@
 
 /* questions to answer:
 how does the Math.abs work?
-what is a ternery opperator?
+what is Math?
+the java script Math object allows you to preform mathmatical tasks
+on numbers.
+in the above example it takes the number given to the function as an argument
+and applies the .abs method to it, which gives the absolute value on the number 
+given as an argument.
+what is a ternery opperator? its a one line if else booleanian operation.
 
 
 */

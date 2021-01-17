32a817704a3edc2d39ae26e80cfe1f178613488b
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-01-17 14:20:46 +0000

    oppositeNumber refactor

--- src/oppositeNumber.js
+++ src/oppositeNumber.js
@@ -1,4 +1,13 @@
-function oppositeNumber(number){
+// function oppositeNumber(number){
 
-  return number > 0 ?  -Math.abs(number) : Math.abs(number);
+//   return number > 0 ?  -Math.abs(number) : Math.abs(number);
+// }
+
+// questions to answer:
+
+
+//alternative
+
+function oppositeNumber(number){
+  return number - (number * 2)
 }

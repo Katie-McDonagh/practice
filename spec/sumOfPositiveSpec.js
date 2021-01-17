276272fbb0fd24265ea576bf0a8e0a053b465235
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-01-17 15:48:59 +0000

    sumOfPositive returns a zero when given all negative intergers

--- spec/sumOfPositiveSpec.js
+++ spec/sumOfPositiveSpec.js
@@ -5,5 +5,9 @@ describe("sumOfPositive", function(){
 
   it ("ignores the negative numbers", function(){
     expect(sumOfPositive([1, -1, 1])).toEqual(2);
+  });
+
+  it ("returns 0 when given all negative integers", function(){
+    expect(sumOfPositive([-1, -1])).toEqual(0);
   })
 })
--- src/sumOfPositive.js
+++ src/sumOfPositive.js
@@ -1,9 +1,11 @@
 function sumOfPositive(array){
   var total = 0
   for(index = 0; index < array.length; index ++){
-    if (array[index] > 0) {
-    total += array[index] 
+    if (array[index] < 0) {
+      array[index] = 0
+   
     }   
+    total += array[index] 
   }
   return total 
 }

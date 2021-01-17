f139a1269cd78717357331a8879d67d1c2c89afa
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-01-17 15:42:20 +0000

    sumOfPositive returns the sum of positive integers

---
+++ spec/sumOfPositiveSpec.js
@@ -0,0 +1,5 @@
+describe("sumOfPositive", function(){
+  it ("returns the sum of positive numbers when given an array of positive integers", function(){
+    expect(sumOfPositive([1,1])).toEqual(2);
+  });
+})
---
+++ src/sumOfPositive.js
@@ -0,0 +1,11 @@
+function sumOfPositive(array){
+  var total = 0
+  for(index = 0; index < array.length; index ++){
+    total += array[index]    
+  }
+  return total 
+}
+
+
+// whats the different ways we can itterate over an array in JS? for, forEach, map? 
+//which to use when?
